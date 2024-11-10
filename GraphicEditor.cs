58962f1c06980c3678932dc5f9a91fe501d1ed98
Ivan <122542790+edvinhubbyy@users.noreply.github.com>
2024-11-10 15:18:58 +0200

    Add files via upload

---
+++ GraphicEditor.cs
@@ -0,0 +1,12 @@
+﻿namespace P02.Graphic_Editor
+{
+    public class GraphicEditor
+    {
+
+        public void Draw(IShape shape)
+        {
+            shape.Draw();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace P02.Graphic_Editor
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            GraphicEditor editor = new GraphicEditor();
+
+            Circle circle = new Circle();
+            Rectangle rectangle = new Rectangle();
+            Square square = new Square();
+
+            editor.Draw(circle);
+            editor.Draw(rectangle);
+            editor.Draw(square);
+
+        }
+    }
+}
